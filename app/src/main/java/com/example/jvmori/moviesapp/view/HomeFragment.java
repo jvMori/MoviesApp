9eeb0808d7ea9fd8cc390354a9b0bd7bbf222a3c
Mori <iwona.moryc92@gmail.com>
2018-12-30 19:31:58 +0100

    fragment home

---
+++ app/src/main/java/com/example/jvmori/moviesapp/view/HomeFragment.java
@@ -0,0 +1,85 @@
+package com.example.jvmori.moviesapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jvmori.moviesapp.R;
+import com.example.jvmori.moviesapp.model.genre.Genre;
+import com.example.jvmori.moviesapp.model.popularMovies.PopularItem;
+import com.example.jvmori.moviesapp.viewModel.GenreViewModel;
+import com.example.jvmori.moviesapp.viewModel.PopularMoviesViewModel;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    private View view;
+    private PopularMovieAdapter popularMovieAdapter;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setPopularMovieAdapter();
+        setGenreViewModel();
+        setPopularMovieViewModel();
+    }
+
+    private void setPopularMovieAdapter(){
+        final RecyclerView recyclerView = view.findViewById(R.id.movieRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setHasFixedSize(true);
+        popularMovieAdapter = new PopularMovieAdapter();
+        recyclerView.setAdapter(popularMovieAdapter);
+    }
+
+    private void setGenreViewModel(){
+        GenreViewModel genreViewModel = ViewModelProviders.of(this).get(GenreViewModel.class);
+        genreViewModel.getData().observe(this, new Observer<List<Genre>>() {
+            @Override
+            public void onChanged(List<Genre> genres) {
+                if (genres == null)
+                    return;
+                popularMovieAdapter.setGenres(genres);
+            }
+        });
+    }
+
+    private void setPopularMovieViewModel(){
+        PopularMoviesViewModel movieViewModel = ViewModelProviders.of(this).get(PopularMoviesViewModel.class);
+        movieViewModel.getAllPopularMovies().observe(this, new Observer<List<PopularItem>>() {
+            @Override
+            public void onChanged(List<PopularItem> movies) {
+                if (movies == null)
+                    return;
+                popularMovieAdapter.setPopularMovies(movies);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/jvmori/moviesapp/view/MainActivity.java
+++ app/src/main/java/com/example/jvmori/moviesapp/view/MainActivity.java
@@ -1,60 +1,18 @@
 package com.example.jvmori.moviesapp.view;
 import android.os.Bundle;
 import com.example.jvmori.moviesapp.R;
-import com.example.jvmori.moviesapp.model.genre.Genre;
-import com.example.jvmori.moviesapp.model.popularMovies.PopularItem;
-import com.example.jvmori.moviesapp.viewModel.GenreViewModel;
-import com.example.jvmori.moviesapp.viewModel.PopularMoviesViewModel;
-import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private PopularMovieAdapter popularMovieAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setPopularMovieAdapter();
-        setGenreViewModel();
-        setPopularMovieViewModel();
     }
 
-    private void setPopularMovieAdapter(){
-        final RecyclerView recyclerView = findViewById(R.id.movieRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-        popularMovieAdapter = new PopularMovieAdapter();
-        recyclerView.setAdapter(popularMovieAdapter);
-    }
-
-    private void setGenreViewModel(){
-        GenreViewModel genreViewModel = ViewModelProviders.of(this).get(GenreViewModel.class);
-        genreViewModel.getData().observe(this, new Observer<List<Genre>>() {
-            @Override
-            public void onChanged(List<Genre> genres) {
-                if (genres == null)
-                    return;
-                popularMovieAdapter.setGenres(genres);
-            }
-        });
-    }
-
-    private void setPopularMovieViewModel(){
-        PopularMoviesViewModel movieViewModel = ViewModelProviders.of(this).get(PopularMoviesViewModel.class);
-        movieViewModel.getAllPopularMovies().observe(this, new Observer<List<PopularItem>>() {
-            @Override
-            public void onChanged(List<PopularItem> movies) {
-                if (movies == null)
-                    return;
-                popularMovieAdapter.setPopularMovies(movies);
-            }
-        });
-    }
 }
