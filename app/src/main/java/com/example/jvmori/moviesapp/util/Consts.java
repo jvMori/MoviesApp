b1adc1f5709370acc72f919157f2f0aa6c801ffc
Mori <iwona.moryc92@gmail.com>
2019-01-10 21:18:21 +0100

    detailed movie view

--- app/src/main/java/com/example/jvmori/moviesapp/util/Consts.java
+++ app/src/main/java/com/example/jvmori/moviesapp/util/Consts.java
@@ -4,6 +4,7 @@ public class Consts {
     public static final String api_key = "6a78422b468a1d74ae224a5747a35666";
     public static final String base_url = "https://api.themoviedb.org/3/";
     public static final String base_poster_url = "http://image.tmdb.org/t/p/w185/";
+    public static final String base_backdrop_url = "http://image.tmdb.org/t/p/w500/";
     public static final int number_of_stars = 5;
     public static final String item_clicked_id = "ItemId";
 }
--- app/src/main/java/com/example/jvmori/moviesapp/view/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/example/jvmori/moviesapp/view/activities/MovieDetailsActivity.java
@@ -7,8 +7,11 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.jvmori.moviesapp.R;
+import com.example.jvmori.moviesapp.model.genre.Genre;
 import com.example.jvmori.moviesapp.model.movie.Movie;
 import com.example.jvmori.moviesapp.model.movieDetails.Cast;
 import com.example.jvmori.moviesapp.model.movieDetails.MovieDetails;
@@ -18,6 +21,9 @@ import com.example.jvmori.moviesapp.viewModel.CastViewModel;
 import com.example.jvmori.moviesapp.viewModel.MovieDetailsViewModel;
 import com.example.jvmori.moviesapp.viewModel.SimilarMoviesViewModel;
 import com.example.jvmori.moviesapp.viewModel.SimilarShowsViewModel;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -38,7 +44,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
             if (movie.getSimilarMovies() == null)
                 setSimilarShowsViewModel(id);
         }
+
     }
+
 
     private void setMovieDetailsViewModel(String movieId){
         MovieDetailsViewModel movieDetailsViewModel = ViewModelProviders.of(this).get(MovieDetailsViewModel.class);
@@ -46,6 +54,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
             @Override
             public void onChanged(MovieDetails movieDetails) {
                 movie.setMovieDetails(movieDetails);
+                setDetailViewUI();
                 //TODO: display data
                 //TODO: loading screen disable
             }
@@ -87,4 +96,37 @@ public class MovieDetailsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private static void loadImage(ImageView view, String imageUrl) {
+        Picasso.get()
+                .load(imageUrl)
+                .placeholder(R.drawable.rounded_bg)
+                .into(view);
+    }
+    private void setDetailViewUI(){
+        ImageView backdrop = findViewById(R.id.backdropImg);
+        ImageView posterImg = findViewById(R.id.posterImg);
+        TextView titleTextView = findViewById(R.id.titleTextView);
+        TextView category = findViewById(R.id.category);
+        TextView tagline = findViewById(R.id.tagline);
+        TextView overview = findViewById(R.id.overview);
+
+        loadImage(backdrop, Consts.base_backdrop_url + movie.getMovieDetails().getBackdropUrl());
+        loadImage(posterImg, Consts.base_poster_url + movie.getMovieDetails().getPoster());
+        titleTextView.setText(movie.getMovieDetails().getTitle());
+
+        StringBuilder categoryTxt= new StringBuilder();
+        for (Genre genre : movie.getMovieDetails().getGenres()) {
+            String txtGenre = genre.getName();
+            List<Genre> txtGenres = movie.getMovieDetails().getGenres();
+            categoryTxt.append(txtGenre);
+            if (!txtGenre.equals(txtGenres.get(txtGenres.size()-1).getName()))
+                categoryTxt.append(" | ");
+        }
+        category.setText(categoryTxt);
+        tagline.setText(movie.getMovieDetails().getTagline());
+        overview.setText(movie.getMovieDetails().getOverview());
+
+    }
+
 }
