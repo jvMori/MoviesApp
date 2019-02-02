44b98a2e8ec2a8f21d89dbfc36d1b661864e53b1
Mori <iwona.moryc92@gmail.com>
2019-02-02 18:28:29 +0100

    Create LibraryViewModel.java

---
+++ app/src/main/java/com/example/jvmori/moviesapp/viewModel/LibraryViewModel.java
@@ -0,0 +1,36 @@
+package com.example.jvmori.moviesapp.viewModel;
+
+import android.app.Application;
+
+import com.example.jvmori.moviesapp.model.library.collection.item.LibraryItem;
+import com.example.jvmori.moviesapp.repository.LibraryRepository;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
+
+public class LibraryViewModel extends AndroidViewModel {
+
+    private MediatorLiveData<List<LibraryItem>> allItems;
+    private LibraryRepository libraryRepository;
+
+    public LibraryViewModel(@NonNull Application application) {
+        super(application);
+        libraryRepository = new LibraryRepository(application);
+        allItems = new MediatorLiveData<>();
+    }
+
+    public LiveData<List<LibraryItem>> getAllItems(){
+        allItems.addSource(libraryRepository.getAllItems(), new Observer<List<LibraryItem>>() {
+            @Override
+            public void onChanged(List<LibraryItem> libraryItems) {
+                allItems.postValue(libraryItems);
+            }
+        });
+        return allItems;
+    }
+}
