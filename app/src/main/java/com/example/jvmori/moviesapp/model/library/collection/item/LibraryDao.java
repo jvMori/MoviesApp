b613dae35a1c8beada0f29897ee62b62e4326aad
Mori <iwona.moryc92@gmail.com>
2019-02-02 16:00:00 +0100

    Update LibraryDao.java

--- app/src/main/java/com/example/jvmori/moviesapp/model/library/collection/item/LibraryDao.java
+++ app/src/main/java/com/example/jvmori/moviesapp/model/library/collection/item/LibraryDao.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 public interface LibraryDao
 {
@@ -17,6 +18,9 @@ public interface LibraryDao
 
     @Query("DELETE FROM libraryItems")
     void deleteAll();
+
+    @Update()
+    void update(LibraryItem item);
 
     @Query("SELECT * FROM libraryItems")
     LiveData<List<LibraryItem>> getAllItems();
